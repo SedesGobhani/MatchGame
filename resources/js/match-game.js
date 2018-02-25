56d373cc4aa6bf43931db8eb30d546aad5010e1d
maseagoe <mark.a.seagoe@intel.com>
2018-02-24 23:19:41 -0800

    Added js for flipping function, but still buggy.

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -41,20 +41,26 @@ MatchGame.generateCardValues = function () {
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
+  $game.data({
+    'flipped': []
+  });
   var color =  [25,55,90,160,220,265,310,360];
   // $game.children('.card').remove();
   $game.empty();
-  var arr = cardValues;
-  var m = cardValues.length; // debug - Math.floor(Math.random() * cardValues.length);
-  for(var n = 0; n < m; n++) {
+  for(var n = 0; n < cardValues.length; n++) {
     var $card = $('<div class="card col-xs-3"></div>');
-    $card.data = {
+    $card.data({ // initial values
         'value': cardValues[n],
         'flipped': false,
-        'color': 'hsl(' + color[cardValues[n]] + ', 85%, 65%)'
-      };
+        'color': 'hsl(' + color[cardValues[n-1]] + ', 85%, 65%)'
+      });
     $game.append($card);
   }
+
+  // card clicked event listener
+  $('.card').click(function() { // listen on all Cards
+    MatchGame.flipCard($(this), $game);  // act only on this one
+  });
 };
 
 /*
@@ -62,12 +68,54 @@ MatchGame.renderCards = function(cardValues, $game) {
   Updates styles on flipped cards depending whether they are a match or not.
  */
 
+// This routine runs when a card gets clicked...
 MatchGame.flipCard = function($card, $game) {
+  if( $card.data('flipped') === true ) { // if already flipped
+    return; //do nothing
+  }
 
+  var flipped = $game.data('flipped'); // this gets used a lot
+
+  $card.data('flipped', true); // flipped = true
+  $card.text($card.data('value'));
+  flipped.push($card); // add card to flipped array
+  $card.css(  // update appearance
+    'background-color',
+    $card.data('color')  // flipped color
+  );
+  $card.css(
+    'color',
+    'rgb(256,256,256)'
+  )
+
+  if( flipped.length === 1) { // if no other cards flipped
+    return; // do nothing
+  }
+
+  for (var n = 0; n < 2; n++) { // for each card in array
+    if( flipped[0] === flipped[1]) { // same value
+      flipped[n].css(  // update appearance
+        'background-color',
+        'rbg(32,64,86)'  // matched bg color
+      );
+      flipped[n].css(
+        'color',
+        'rgb(204,204,204)' // matched fg color
+      );
+    } else { // other flipped card value different
+      flipped[n].css(  // update appearance
+        'background-color',
+        'rgb(153,153,153)'  // unflipped bg color
+      );
+      flipped[n].text('');
+      flipped[n].data('flipped', false);
+    }
+  }
+  $game.data('flipped', []); // empty flipped array
 };
 
 /*
-Improvements:
+Possible improvements:
 
 Time delay (1/2 second) to transition matched cards to grey.
 Reset button (instead of browser refresh).
